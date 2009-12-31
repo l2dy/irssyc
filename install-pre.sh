3d0af70132cfb21700f19fbc6f8324b9301e9d65
Gabor Adam Toth <tg@x-net.hu>
2009-12-31 14:56:00 +0100

    install script fixes

--- install-pre.sh
+++ install-pre.sh
@@ -4,6 +4,9 @@ base=`pwd`
 irssi="$HOME/.irssi"
 mode='u=rw,go=r'
 
+irssi_ver='0.8.14'
+perlpsyc_ver='0.26'
+
 echo 'This will make and install all prerequisites for irssi-psyc'
 echo -n 'continue? [Y/n] '
 read cont
@@ -12,18 +15,18 @@ if !([ "$cont" == "y" ] || [ "$cont" == "yes" ] || [ "$cont" == "" ]); then exit
 echo -n 'download sources? [Y/n] '
 read cont
 if ([ "$cont" == "y" ] || [ "$cont" == "yes" ] || [ "$cont" == "" ]); then
-  wget -c http://irssi.org/files/irssi-0.8.12.tar.gz http://perl.psyc.eu/files/perlPSYC-0.26.zip
+  wget -c http://irssi.org/files/irssi-$irssi_ver.tar.gz http://perl.psyc.eu/files/perlPSYC-$perlpsyc_ver.zip
 fi
 echo -n 'unpack sources? [Y/n] '
 read cont
 if ([ "$cont" == "y" ] || [ "$cont" == "yes" ] || [ "$cont" == "" ]); then
-  tar zxvf irssi-0.8.12.tar.gz
-  unzip perlPSYC-0.26.zip
+  tar zxvf irssi-$irssi_ver.tar.gz
+  unzip perlPSYC-$perlpsyc_ver.zip
 fi
 echo -n 'patch sources? [Y/n] '
 read cont
 if ([ "$cont" == "y" ] || [ "$cont" == "yes" ] || [ "$cont" == "" ]); then
-  patch -p0 -d irssi-0.8.12 <irssi-0.8.12-tg.patch
+  patch -p0 -d irssi-0.8.12 <irssi-$irssi_ver-tg.patch
   #patch -p0 -d perlpsyc <perlPSYC-0.25-tg.patch
 fi
 echo -n "install perlPSYC to $irssi/lib? [Y/n] "
@@ -36,7 +39,7 @@ fi
 echo -n 'make irssi? [Y/n] '
 read cont
 if ([ "$cont" == "y" ] || [ "$cont" == "yes" ] || [ "$cont" == "" ]); then
-  cd $base/irssi-0.8.12
+  cd $base/irssi-$irssi_ver
   ./configure --with-proxy --with-bot --with-socks --enable-ssl --enable-ipv6 --with-gc
   make
 
